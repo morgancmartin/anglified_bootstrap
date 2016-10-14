5ef2b60fc7621c2dcb679d81f623a8a08492334c
Adrian Mui <adrian.mui@gmail.com>
2016-10-14 03:08:25 -0500

    added initial/optional plugins, fullPagejs, setup controller, designing user_select_templates_page, setup angular(yet to use)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def include_javascript (file)
+    s = " <script type=\"text/javascript\">" + render(:file => file) + "</script>"
+    puts s
+    content_for(:head, raw(s))
+  end
 end
