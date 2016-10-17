59d819fbd951abcbfb79870492343c20582fd876
Adrian Mui <adrian.mui@gmail.com>
2016-10-17 14:22:47 -0500

    setup models Html, Style, Javascript, and created seeds to test

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:github]
 
+  has_many :htmls
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
