38790f2ab819a0892648677a458709629556653f
cjvirtucio87 <cjv287@gmail.com>
2016-10-17 16:21:47 -0700

    Finished blog preview basic structure

---
+++ app/assets/javascripts/services/resource_service.js
@@ -0,0 +1,20 @@
+// For choosing a resource. Likely to be used in Johnny's SubmitService.
+app.factory('ResourceService', ['_', function (_) {
+
+  var srv = {};
+  var _data = {
+    cached: [],
+    type: 'resource',
+  };
+
+  srv.addResource = function (resource) {
+    _data.cached.push(resource);
+    return Promise.resolve(_data);
+  };
+
+  srv.all = function (resource) {
+    return Promise.resolve(_data);
+  };
+
+  return srv;
+}]);
