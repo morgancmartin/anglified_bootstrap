8d992421a046ac4947d68ed4aaa735b6c92e0a32
cjvirtucio87 <cjv287@gmail.com>
2016-10-17 18:30:50 -0700

    removed unneeded code

--- app/assets/javascripts/controllers/editApp/comments_preview_ctrl.js
+++ app/assets/javascripts/controllers/editApp/comments_preview_ctrl.js
@@ -6,10 +6,5 @@ app.controller('CommentsPreviewCtrl', ['ResourceService', function(ResourceServi
     ResourceService.addResource('comments');
   };
 
-  vm.random = function () {
-    var rand = parseInt(Math.random(500)*1000);
-    return rand % 2 === 0 ? 'love' : 'hate';
-  };
-
   vm.comments = [1,2,3];
 }]);
