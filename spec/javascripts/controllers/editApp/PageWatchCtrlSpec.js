781ce62d6a8be862264f591e70b4893004241ee3
cjvirtucio87 <cjv287@gmail.com>
2016-10-18 08:39:29 -0700

    Added nextState and createSlide tests

--- spec/javascripts/controllers/editApp/PageWatchCtrlSpec.js
+++ spec/javascripts/controllers/editApp/PageWatchCtrlSpec.js
@@ -1,7 +1,7 @@
 describe('PageWatchCtrl', function() {
   beforeEach(module('editApp'));
 
-  var $controller, $rootScope, $event, $scope, ctrl;
+  var $controller, $rootScope, $event, $scope, ctrl, element;
 
   beforeEach(inject(function(_$controller_, _$rootScope_) {
     $controller = _$controller_;
@@ -11,17 +11,40 @@ describe('PageWatchCtrl', function() {
     // Setting up $scope and controller.
     $scope = {};
     ctrl = $controller('PageWatchCtrl', {$scope: $scope});
-    // mock the $broadcast method
+
+    // Mock the $broadcast method
     $scope.$broadcast = function () {};
     spyOn($scope,'$broadcast');
+
+    // Selected section.
+    element = "<section data-slide='1'></section>";
+    $event.currentTarget = element;
   }));
 
   describe('createSlide', function () {
     it('adds a state', function () {
-      var element = "<section data-slide='1'></section>";
-      $event.currentTarget = element;
       $scope.createSlide($event);
       expect($scope.states.length).toEqual(2);
+    });
+
+    it('takes you to the recently created slide', function () {
+      $scope.createSlide($event);
+      expect($scope.count).toEqual(1);
+    });
+  });
+
+  describe('nextState', function () {
+    it("brings you to the start when you're at the last slide", function () {
+      $scope.createSlide($event);
+      $scope.nextState();
+      expect($scope.count).toEqual(0);
+    });
+
+    it("brings you to the next slide when you're not at the end", function () {
+      $scope.createSlide($event);
+      $scope.nextState();
+      $scope.nextState();
+      expect($scope.count).toEqual(1);
     });
   });
 });
