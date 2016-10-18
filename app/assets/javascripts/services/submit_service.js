0c8b5e3434892831cb94169ae2435970d1ebe4e1
jsteenb2 <jonathansteenbergen@gmail.com>
2016-10-17 21:25:05 -0700

    integrated file writing and front-end api calls

--- app/assets/javascripts/services/submit_service.js
+++ app/assets/javascripts/services/submit_service.js
@@ -5,26 +5,32 @@ app.factory('submitService', ["_", "Restangular", function(_, Restangular) {
     var $body = angular.element('body').clone();
     $body = _removeEditor($body);
     output = _slideSplice($body, slideStack);
-    // console.log(output);
+    console.log(output);
     // return Promise.resolve(output);
     return Restangular.all("templates").post(output);
   };
 
   var _slideSplice = function($obj, collection){
     var output = {
-      head: angular.element('head').html(),
+      head: angular.element('head').prop('outerHTML'),
       body: {
-        withEdits: angular.element('body').clone().html()
+        withEdits: $obj.prop('outerHTML')//,
+        // final: {
+        //   nav:$obj.find('nav').prop('outerHTML'),
+        //   footer: $obj.find('footer').prop('outerHTML')
+        // }
       }
     };
 
     var $bodyWrapper = angular.element("<div full-page options=''>");
 
     _.each(collection, function(slide){
+      // console.log(slide);
       var $sectionWrapper = angular.element("<div class='section'>");
-      var $slides = $obj.children("[data-slide='" + slide +"']");
+      var $slides = $obj.find("[data-slide='" + slide +"']").clone();
 
       $slides.each(function(index, element){
+        // console.log($sectionWrapper);
         var $element = _cleanAttrs(element);
         $element = _addAttrs($element);
         $sectionWrapper.append($element);
@@ -33,6 +39,8 @@ app.factory('submitService', ["_", "Restangular", function(_, Restangular) {
     });
     var newBody = _newBody($bodyWrapper);
     output.body.final = newBody;
+    // output.body.final.body = $bodyWrapper.prop('outerHTML');
+    console.log(newBody);
     return output;
   };
 
@@ -41,8 +49,10 @@ app.factory('submitService', ["_", "Restangular", function(_, Restangular) {
                 .clone();
     $body = _removeSectionsTools($body);
     $body = _removeEditor($body);
-    $body.find('nav').after($wrapper);
-    return $body.html();
+    $nav = $body.find('nav');
+    $nav.after($wrapper);
+    // $wrapper.insertAfter($body.find('nav'));
+    return $body.prop('outerHTML');
   };
 
   var _removeSectionsTools = function($obj){
