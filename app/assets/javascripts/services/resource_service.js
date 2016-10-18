d280cdce06cc9b7603e267edda16645d89465c81
cjvirtucio87 <cjv287@gmail.com>
2016-10-17 18:30:10 -0700

    fixed digest issue

--- app/assets/javascripts/services/resource_service.js
+++ app/assets/javascripts/services/resource_service.js
@@ -9,6 +9,7 @@ app.factory('ResourceService', ['_', function (_) {
 
   srv.addResource = function (resource) {
     _data.cached.push(resource);
+    console.log(_data);
     return Promise.resolve(_data);
   };
 
