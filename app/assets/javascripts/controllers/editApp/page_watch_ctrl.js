83be63e36b6ff9df097db2da3ff107e0b1e19c08
Adrian Mui <adrian.mui@gmail.com>
2016-10-19 11:22:06 -0500

    transfered tinymce to service

--- app/assets/javascripts/controllers/editApp/page_watch_ctrl.js
+++ app/assets/javascripts/controllers/editApp/page_watch_ctrl.js
@@ -1,17 +1,29 @@
 app.controller('PageWatchCtrl',
-['$scope', '$rootScope', "_", 'submitService',
-function($scope, $rootScope, _, submitService){
+['$scope', '$rootScope', "_", 'submitService', 'tinyMCEService',
+function($scope, $rootScope, _, submitService, tinyMCEService){
+
+  $scope.editStates = {
+    section: false,
+    textbox: false,
+    tinymce: false
+  };
+
   var elements = angular.element('body *');
 
-  for (var i =1; i < ( 1 + elements.length ); i++){
-    angular.element(elements[i]).attr('data-id', i);
-  }
+  // for (var i =1; i < ( 1 + elements.length ); i++){
+  //   angular.element(elements[i]).attr('data-id', i);
+  // }
   // compare element's id with currentstate; if match { show }
 
   $scope.page = {};
-
   $scope.states = ['home'];
   $scope.count = 0;
+
+
+  // Listener for toggle events in the sidebar.
+  $rootScope.$on('sidebar.toggled', function (ev, editStates) {
+    angular.copy(editStates, $scope.editStates);
+  });
 
   // Clicking 'make a new slide' on a section/header/footer
   // should take it out of the main page and give it its own slide.
@@ -37,19 +49,6 @@ function($scope, $rootScope, _, submitService){
     $scope.$broadcast('states.nextState', $scope.states[$scope.count] );
   };
 
-  // Edit states of the different parts.
-  $scope.editStates = {
-    section: false,
-    textbox: false,
-    tinymce: false
-
-  };
-
-  // Listener for toggle events in the sidebar.
-  $rootScope.$on('sidebar.toggled', function (ev, editStates) {
-    angular.copy(editStates, $scope.editStates);
-  });
-
   // Checkbox values for sections.
   // Toggle true/false
   $scope.checkboxValues = {};
@@ -71,6 +70,9 @@ function($scope, $rootScope, _, submitService){
     });
   };
 
+
+  debugger;
+
   /*
   ----------------------------------------------------
   Tiny MCE
@@ -79,73 +81,33 @@ function($scope, $rootScope, _, submitService){
 
   $scope.previousId;
   $scope.mce = function (event) {
+    tinyMCEService.clearEditors();
     var id;
-
-    for (var i = tinymce.editors.length - 1 ; i > -1 ; i--) {
-      var ed_id = tinymce.editors[i].id;
-      tinyMCE.execCommand("mceRemoveEditor", true, ed_id);
-    }
-
-
-      if($scope.previousId) {
-        var nested_targ = event.target;
-        while (!nested_targ.class && (nested_targ.class !== "textable" )) {
-          if (nested_targ.id) {
-            id = nested_targ.id;
-            break;
-          }
-          nested_targ = angular.element(nested_targ).parent()[0];
+    // handle edge cases to select textable
+    if($scope.previousId) {
+      var nested_targ = event.target;
+      while (!nested_targ.class && (nested_targ.class !== "textable" )) {
+        if (nested_targ.id) {
+          id = nested_targ.id;
+          break;
         }
-      } else {
-        id = event.target.id;
-        $scope.previousId = id;
+        nested_targ = angular.element(nested_targ).parent()[0];
       }
-
-      tinymce.init({
-        selector: ('#' + id),
-        plugins: 'link image code wordcount',
-        toolbar: 'mybutton | myimage | close | undo redo | bold italic | alignleft aligncenter alignright | code',
-        theme: "inlite",
-        inline: true,
-        menubar: true,
-        setup: function (editor) {
-          editor.addButton('mybutton', {
-            text: 'ClickMe',
-            icon: false,
-            onclick: function () {
-              editor.insertContent('<button class="btn btn-info">DoNothing</button>');
-            }
-          });
-          editor.addButton('close', {
-            text: 'Exit',
-            icon: false,
-            onclick: function() {
-              editor.destroy();
-            }
-          });
-          editor.addButton('myimage', {
-            text: 'Image',
-            icon: false,
-            onclick: function() {
-              editor.insertContent('<img style="height: 50px" src="https://cdn0.iconfinder.com/data/icons/iconshock_guys/512/andrew.png"></img>');
-            }
-          });
-        },
-        content_css: [
-          'https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.1.1/css/mdb.min.css'
-        ]
-      });
-  };
-
+    } else {
+      id = event.target.id;
+      $scope.previousId = id;
+    }
+    tinyMCEService.initMCE(id);  
+  }
   //sets a listener to 'textable' class tags
   // builds tinyMCE editor and hides selected tag
   $scope.$watch('editStates.tinymce', function(newVal) {    
     if (newVal) {
       angular.element('.textable').on('click', $scope.mce);
     } else {
-      tinymce.remove('.textable');
+      tinyMCEService.clearEditors();
       angular.element('.textable').off('click', $scope.mce);
-      console.log("tinyMceListeners and Editors are gone");
+      console.log("tinyMCE listeners OFF");
     }
   });
 
---
+++ app/assets/javascripts/services/tinymce_service.js
@@ -0,0 +1,49 @@
+app.factory('tinyMCEService',
+['_', function( _ ) {
+
+  var stub = {};
+
+  stub.clearEditors = function () {
+    tinymce.remove('.textable');
+  };
+
+  stub.initMCE = function(id) {
+    return tinymce.init({
+      selector: ('#' + id),
+      plugins: 'link image code wordcount',
+      toolbar: 'mybutton | myimage | close | undo redo | bold italic | alignleft aligncenter alignright | code',
+      theme: "inlite",
+      inline: true,
+      menubar: true,
+      setup: function (editor) {
+        editor.addButton('mybutton', {
+          text: 'ClickMe',
+          icon: false,
+          onclick: function () {
+            editor.insertContent('<button class="btn btn-info">DoNothing</button>');
+          }
+        });
+        editor.addButton('close', {
+          text: 'Exit',
+          icon: false,
+          onclick: function() {
+            editor.destroy();
+          }
+        });
+        editor.addButton('myimage', {
+          text: 'Image',
+          icon: false,
+          onclick: function() {
+            editor.insertContent('<img style="height: 50px" src="https://cdn0.iconfinder.com/data/icons/iconshock_guys/512/andrew.png"></img>');
+          }
+        });
+      },
+      content_css: [
+        'https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.1.1/css/mdb.min.css'
+      ]
+    });
+  };
+
+
+  return stub;
+}]);
