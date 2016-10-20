426579210d40e96684cb51fa7b2463944c616c0b
jsteenb2 <jonathansteenbergen@gmail.com>
2016-10-19 19:57:11 -0700

    removed state error reporter

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -31,9 +31,10 @@ var app = angular.module('editApp',
 ['restangular', 'Devise', 'ui.bootstrap', "ui.router", "ui.tinymce"]);
 
 // Error logging.
-app.run([function($rootScope){
-  $rootScope.$on("$stateChangeError", console.log.bind(console));
-}]);
+// COMMENTED OUT FOR PRODUCTION
+// app.run([function($rootScope){
+//   $rootScope.$on("$stateChangeError", console.log.bind(console));
+// }]);
 
 app.factory('_', ['$window', function($window){
   return $window._;
