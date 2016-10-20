03e65157181e382fcfb2538475b36c8449c2cb92
Adrian Mui <adrian.mui@gmail.com>
2016-10-20 00:25:07 -0500

    changing tinymce depending ondifferent node/tag type

--- app/assets/javascripts/services/tinymce_service.js
+++ app/assets/javascripts/services/tinymce_service.js
@@ -5,6 +5,7 @@ function( _, userEditService, $timeout) {
   var stub = {};
 
   var _previousNode;
+  var _previousId;
 
   stub.getPreviousNode = function() {
     return _previousNode;
@@ -17,8 +18,6 @@ function( _, userEditService, $timeout) {
   stub.clearEditors = function () {
     tinymce.remove('.textable');
   };
-
-  var _previousId;
 
   stub.callMCE = function(event) {
     stub.clearEditors();
@@ -38,31 +37,57 @@ function( _, userEditService, $timeout) {
       id = event.target.id;
       _previousId = id;
     }
-
+    // change the element targetted on the dom
     var change = nested_targ || event.target;
     change = angular.element(change).clone();
     stub.setPreviousNode(change);
 
-    stub.initMCE(id);
+    // utilizes different set of tinyMCEs depending on type of node/Domelement
+    stub.whichMCE(change.get(0).tagName, id);
+    // t
   };
 
-  stub.initMCE = function(id) {
+  stub.whichMCE = function(tagType, id) {
+    switch(tagType) {
+      case 'A':
+        stub.miniMCE(id);
+        break;
+      case 'IMG':
+        console.log('hi image not rdy 4 u yet');
+        break;
+    default:
+        stub.defaultMCE(id);
+        break;
+    }
+  };
+
+  stub.miniMCE = function(id) {
+    tinymce.init({
+      selector: ('#' + id),
+      
+      theme: "inlite",
+      inline: true,
+      paste_data_images: true,
+      plugins: 'image link paste contextmenu textpattern autolink',
+      insert_toolbar: 'quickimage',
+      selection_toolbar: 'bold italic | quicklink',
+    });
+  };
+
+  stub.defaultMCE = function(id) {
     tinymce.init({
       selector: ('#' + id),
       plugins: 'link image code wordcount',
-      toolbar: 'mybutton | myimage | close | undo redo | bold italic | alignleft aligncenter alignright | code',
-      theme: "inlite",
-      inline: true,
-      menubar: true,
+      toolbar: 'myimage | close | undo redo | bold italic | alignleft aligncenter alignright | code',
+      menubar: false,
       setup: function (editor) {
-        editor.addButton('mybutton', {
-          text: 'ClickMe',
-          icon: false,
-          onclick: function () {
-            editor.insertContent('<button class="btn btn-info">DoNothing</button>');
-          }
-        });
-
+        // editor.addButton('mybutton', {
+        //   text: 'ClickMe',
+        //   icon: false,
+        //   onclick: function () {
+        //     editor.insertContent('<button class="btn btn-info">DoNothing</button>');
+        //   }
+        // });
         editor.addButton('close', {
           text: 'Exit',
           icon: false,
@@ -80,7 +105,6 @@ function( _, userEditService, $timeout) {
       }
     });
   };
-
 
   return stub;
 }]);
